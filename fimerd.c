0117c3d121579d39fd607177df611e2ad61f28e9
Amit Kulkarni <kulkarni.am@husky.neu.edu>
2016-11-10 03:18:55 -0500

    Receive, parse, poll, execute, delete done
    1. Functionality to receive a standard message format added
    2. Functionality added to parse a message and store it in required
       data structure
    3. Poll through existing jobs, execute expired jobs and delete them

--- fimerd.c
+++ fimerd.c
@@ -15,6 +15,7 @@
  * If a job has expired, action is taken and job is removed from queue
  */
 
+#include <ctype.h>
 #include <sys/socket.h>
 #include <sys/types.h>
 #include <linux/limits.h>
@@ -36,75 +37,149 @@
 #define LISTENING_PORT 51515
 #define BACKLOG	2
 #define MESSAGE_BUFFER_SIZE 1024
+#define MESSAGE_SEPARATOR ":"
+/* 	10 characters can fit: 4294967296 
+ 	1 character to specify 'h','s','m','d','M','y','w'
+	1 character for NULL TERMINATOR	*/
+#define DURATION_SIZE	12
+#define OPCODE_SIZE	3	/* Allow 99 opcodes and 1 NULL TERMINATOR	*/
+#define MESSAGE_PARAMETERS_SIZE 255
+#define DEFAULT_TIME_UNIT 'm'
 
+/* MACRO FUNCTIONS	*/
+#define MINUTES_TO_SECONDS(m) (m * 60)
+#define HOURS_TO_SECONDS(h) (h * MINUTES_TO_SECONDS(60))
+#define DAYS_TO_SECONDS(d) (d * HOURS_TO_SECONDS(24))
+#define WEEKS_TO_SECONDS(w) (w * DAYS_TO_SECONDS(7))
+#define MONTHS_TO_SECONDS(months) (months * WEEKS_TO_SECONDS(4))
+#define YEARS_TO_SECONDS(y) (y * MONTHS_TO_SECONDS(12))
+
+int global_guy=1;
+struct job *head = NULL;
+struct timespec current_time;
 /**********************************************************************/
 /*	Operation methods to be moved to a header file					  */
 /**********************************************************************/
-void process_chmod(char *params)
+void process_chmod(char *filepath, char *params)
 {
-	syslog(LOG_INFO, "RIP, we will process and add to queue\n");
+	syslog(LOG_INFO, "RIP, Let's do the job\n");
+	syslog(LOG_INFO, "Let's chmod the %s file \n", filepath);	
+}
+
+char get_duration_unit(char *received_duration)
+{
+	while(!isalpha(*received_duration) && *received_duration != '\0'){
+		received_duration++;
+	}
+	return (*received_duration == '\0')?
+			DEFAULT_TIME_UNIT:
+			*received_duration;
 }
 /**********************************************************************/
-void parse_and_add()
+unsigned int get_duration_seconds(unsigned int duration, char unit)
 {
-	struct job_data *data = malloc(sizeof(struct job_data));
-	struct job_duration *duration = malloc(sizeof(struct job_duration));
+	unsigned int duration_in_seconds;
+	switch(unit){
+		case 's':	duration_in_seconds = duration;
+					break;
+		case 'm':	duration_in_seconds = MINUTES_TO_SECONDS(duration);
+                    break;
+		case 'h':	duration_in_seconds = HOURS_TO_SECONDS(duration);
+                    break;
+		case 'd':	duration_in_seconds = DAYS_TO_SECONDS(duration);
+                    break;
+		case 'M':	duration_in_seconds = MONTHS_TO_SECONDS(duration);
+                    break;
+		case 'w':	duration_in_seconds = WEEKS_TO_SECONDS(duration);
+                    break;
+		case 'y':	duration_in_seconds = YEARS_TO_SECONDS(duration);
+                    break;
+		default:	exit(EXIT_FAILURE);
+	}
+	return duration_in_seconds;
+}
 
-	/*  Sample Initializations  */
-	char received_filepath[PATH_MAX] = "/home/user/somefile";
-	char received_params[MESSAGE_BUFFER_SIZE] = "0700";
-
-	/*  Linked list head allocation */
-	struct job *head = NULL;
-	
-	/*  Create a new job for every new request  */
+void assign_job(char *job_message)
+{
+	/*
+		Timer structs	
+	*/
+	struct timespec job_assign_time, job_expiry_time;
+	/*	
+		Generic function that delegates the job to different
+		functions depending on opcode
+	*/
 	struct job *new = malloc(sizeof(struct job));
+
+	/*
+		Well, strtok_r() has been generous enough to do this job.
+		All we have to do it just pass char * pointer and it will
+		take care of the rest of the job
+	char received_filepath[PATH_MAX];
+	char received_duration[DURATION_SIZE];
+	char received_opcode[OPCODE_SIZE];
+	char received_parameters[MESSAGE_PARAMETERS_SIZE];
+	*/
+	char *received_filepath, *received_duration, *received_opcode;
+	char *received_parameters;
+	unsigned int opcode, duration, expiry_time;
+	char duration_unit;
+	char *saveptr = NULL;
+
+	/*	strtok_r() modifies its first argument, save a backup	*/
+	char *received_message = strdup(job_message);
+
+	struct job_data *data = malloc(sizeof(struct job_data));
+//	struct job_duration *duration = malloc(sizeof(struct job_duration));
 	
+	received_filepath = strtok_r(received_message, MESSAGE_SEPARATOR, &saveptr);
+	received_duration = strtok_r(NULL, MESSAGE_SEPARATOR, &saveptr);
+	received_opcode   = strtok_r(NULL, MESSAGE_SEPARATOR, &saveptr);
+	received_parameters = strtok_r(NULL, MESSAGE_SEPARATOR, 
+								   &saveptr);
+	opcode   = (unsigned int)strtol(received_opcode, NULL, 10);
+	duration = (unsigned int)strtol(received_duration, NULL, 10);
+	duration_unit = get_duration_unit(received_duration);
+
+	expiry_time = get_duration_seconds(duration, duration_unit);
+
 	/*  Initialize job_data struct  */
-	data->opcode = OCHMOD;
 	data->filepath = malloc(strlen(received_filepath)+1);   /* Remember +1 */
 	strcpy(data->filepath, received_filepath);
-	data->params= malloc(strlen(received_params)+1);    /* Remember +1 */
-	strcpy(data->params, received_params);
-	data->job_worker = process_chmod;
+	syslog(LOG_INFO, "Received filepath : %s\n",data->filepath);
+	data->params= malloc(strlen(received_parameters)+1);    /* Remember +1 */
+	strcpy(data->params, received_parameters);
 	
-	/*  Initialize job_duration struct  */
-	duration->unit = 'm';
-	duration->time = 10;
+	switch(opcode){
+		case OCHMOD:
+			data->opcode = OCHMOD;
+			data->job_worker = process_chmod;
+			break;
+		default:	exit(EXIT_FAILURE);	
+	}
 	
-	new->delete = 0;
-	new->duration = duration;
+	clock_gettime(CLOCK_MONOTONIC, &job_assign_time);
+	job_expiry_time = job_assign_time;
+	job_expiry_time.tv_sec += expiry_time;
+
+	/*  
+		Initialize job_duration struct 
+	duration->unit = duration_unit;
+	duration->time = duration;
+	*/
+	
+	new->delete = 0;	/* Job hasn't expired yet 	*/
 	new->data = data;
+	new->job_expiry = job_expiry_time;
 	new->next = NULL;
-	
-	struct job *new_2 = malloc(sizeof(struct job));
-	new_2->delete = 1;
-	new_2->duration = duration;
-	new_2->data = data;
-	new_2->next = NULL;
-	
 	append_jobs_list(&head, new);
-	append_jobs_list(&head, new_2);
 	print_jobs_list(head);
 	
-	/*	
-		You have to free() the allocated memory in exact reverse 
-		order of how it was allocated using malloc()	
-	*/
 	/*   Free the heap memory   */
-	struct job *tmp;
-
-	while (head != NULL){
-		tmp = head;
-		head = head->next;
-		free(tmp);
-    }
-	free(duration);
-	free(data);
+//	free(data);
 }
 
 
-int global_guy=1;
 
 void display_error_exit()
 {
@@ -156,31 +231,45 @@ void *thread_job(void *ptr){
 			break;
 		}
 		syslog(LOG_INFO, "Received a message on socket\n");
+		clock_gettime(CLOCK_MONOTONIC, &current_time);
+	    syslog(LOG_INFO, "Current time: %d\n",current_time.tv_sec);
 	
 		/*	Function to parse and add job to queue	*/	
-		parse_and_add();
-
-		clock_gettime(CLOCK_MONOTONIC, &job_assign_time);
-		now = job_assign_time;
-		now.tv_sec += 5;
-		fprintf(stdout, "Start time: %lu\n",job_assign_time.tv_sec);
-		fprintf(stdout, "End time: %lu\n",now.tv_sec);
+		assign_job(buffer);
 		sleep(5);
-		struct timespec ts_current;
-		struct timespec ts_remaining;
-		clock_gettime(CLOCK_MONOTONIC, &ts_current);
-		ts_remaining.tv_sec = now.tv_sec - ts_current.tv_sec;
-
-		if(ts_remaining.tv_sec <= 0){
-			syslog(LOG_INFO, "Time's up! Its time to perform operation..\n");
-			syslog(LOG_INFO, "Time elapsed, happy times now\n");
-			break;
-		}
 		memset(buffer, 0, sizeof(buffer));
 	}
 	syslog(LOG_INFO, "Updating global guy now..\n");
 	global_guy++;
 	close(listen_fd);
+}
+
+void execute_eligible_jobs(){
+	/*	
+		1.	Run through jobs list 
+		2.	Perform jobs that have expired
+		3.	Unlink expired job from list
+	*/
+	clock_gettime(CLOCK_MONOTONIC, &current_time);
+	syslog(LOG_INFO, "Current time: %d\n",current_time.tv_sec);
+	struct job *current = head;
+	struct job *next_job;
+	while(current != NULL){
+		next_job = current->next;
+		if(current->job_expiry.tv_sec <= current_time.tv_sec){
+			syslog(LOG_INFO,"Time to delete job!\n");
+			/*	Eligible job to be executed	*/
+			current->data->job_worker(current->data->filepath, 
+									  current->data->params);
+		
+			/* Job done. Remove the job	*/
+			delete_job(&head, current);
+			current = next_job;
+			continue;
+		}
+		current = current->next;
+	}
+
 }
 
 int main(int argc, char *argv[]) {
@@ -235,10 +324,33 @@ int main(int argc, char *argv[]) {
 	/*  Create a thread that updates global guy */
 	pthread_create(&server_thread, NULL, thread_job, NULL);
 	
-	/* A useless payload to demonstrate */
 	while (1) {
 	    sleep(2);
+		syslog(LOG_INFO, "Checking for head...\n");
+		if(head != NULL){
+			syslog(LOG_INFO, "head is not NULL, continuing check..\n");
+			/*	There are some jobs in the linkedlist queue	*/
+			execute_eligible_jobs();
+		}
+		/*	For temporary testing	*/
+		/*	
+			You have to free() the allocated memory in exact reverse 
+			order of how it was allocated using malloc()	
+		*/
+		/*
+		else{
+			struct job *tmp;
+			while (head != NULL){
+				tmp = head;
+				head = head->next;
+				free(tmp);
+ 			}
+		}
+		*/
 	    if(global_guy % 2 == 0){
+	        clock_gettime(CLOCK_MONOTONIC, &current_time);
+    	    syslog(LOG_INFO, "Current time: %d\n",current_time.tv_sec);
+
 	        syslog(LOG_INFO, "Found an even number!\n");
 	    }
 	}
