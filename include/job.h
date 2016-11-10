ca34cf00acbaae7e5b7169a7918e2aee8b1296d3
Amit Kulkarni <kulkarni.am@husky.neu.edu>
2016-11-10 16:48:25 -0500

    Removed unnecessary duration data structures

--- include/job.h
+++ include/job.h
@@ -1,16 +1,6 @@
 #ifndef _JOB_H_
 #define _JOB_H_
 
-/*	Well, this surely looks cute, but I don't think
-	we actually need this info going further.
-	All the information that is requires is the 
-	expiry time	*/
-struct job_duration
-{
-    char unit;
-    unsigned int time;
-};
-
 struct job_data
 {
 	/* A struct that represents every kind of job */
@@ -25,7 +15,6 @@ struct job
 {
     unsigned int delete:1;	/* Flag to keep track of expiry */
     struct job_data *data;
-//    struct job_duration *duration;
 	struct timespec job_expiry;
     struct job *next;
 };
